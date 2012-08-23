7544a17f6401a9b5eeb7e8dab40d35cdd045939e
Sumon Sadhukhan <sumon.sadhukhan8@gmail.com>
2012-08-24 03:10:23 +0530

    Update src/main/java/udhc/prediction/Find.java
    minor change

--- src/main/java/udhc/prediction/Find.java
+++ src/main/java/udhc/prediction/Find.java
@@ -16,6 +16,8 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 /**
  * Servlet implementation class for Servlet: Search
+ * It takes "Text" as get request and generates a XML file as a output.
+ * in the portion "passage", the main content is written.
  *
  */
   public class Find extends HttpServlet {
@@ -41,22 +43,18 @@ import javax.servlet.http.*;
       //creating a new instance of a DOM to build a DOM tree.
       Document doc = docBuilder.newDocument();
 
-      //filewalk f= new filewalk();
-        //f.walk("./webapps/ROOT/WEB-INF/");
       out= response.getWriter();
 
-        String Text=request.getParameter("Text");
+      String Text=request.getParameter("Text");
       MedicalWord m = new MedicalWord();
       String[] medical=m.getMedicalWord();
-        predict prediction= new predict( Text,medical );
-    String[] output=prediction.getResult();
+      predict prediction= new predict( Text,medical );
+      String[] output=prediction.getResult();
     
-      //String[] medical=prediction.getMedicalWord();
-      //out.println("length: of medical word"+medical.length);
-    parser p=new parser("http://open-archive.highwire.org/handler?verb=ListRecords&metadataPrefix=oai_dc&set=casereports","dc:description");
-   //out.write("<channel>");
+      parser p=new parser("http://open-archive.highwire.org/handler?verb=ListRecords&metadataPrefix=oai_dc&set=casereports","dc:description");
+   
       
-Element root = doc.createElement("Company");
+      Element root = doc.createElement("Company");
       //adding a node after the last child node of the specified node.
       doc.appendChild(root);
 
@@ -78,24 +76,23 @@ Element root = doc.createElement("Company");
         continue;
       }
       s.reset();
-        s.StartSearch(parsed[i], output);
-  Element child = doc.createElement("itempassage");
-  root.appendChild(child);
-  Element child1 = doc.createElement("passage1");
-    child.appendChild(child1);
+      s.StartSearch(parsed[i], output);
+      Element child = doc.createElement("itempassage");
+      root.appendChild(child);
+      Element child1 = doc.createElement("passage1");
+      child.appendChild(child1);
 
-    Text text = doc.createTextNode(parsed[i]);
-    child1.appendChild(text);
-  Element element = doc.createElement("iii");
-  child.appendChild(element);
+      Text text = doc.createTextNode(parsed[i]);
+      child1.appendChild(text);
+      Element element = doc.createElement("iii");
+      child.appendChild(element);
 
-    Text text1 = doc.createTextNode(""+s.getCount());
-    element.appendChild(text1);
+      Text text1 = doc.createTextNode(""+s.getCount());
+      element.appendChild(text1);
 
     }
-      TransformerFactory factory = TransformerFactory.newInstance();
+    TransformerFactory factory = TransformerFactory.newInstance();
     Transformer transformer = factory.newTransformer();
-   
     transformer.setOutputProperty(OutputKeys.INDENT, "yes");
 
     // create string from xml tree
@@ -105,8 +102,7 @@ Element root = doc.createElement("Company");
     transformer.transform(source, result);
     String xmlString = sw.toString();
     out.write(xmlString);
- //out.write("</channel>");  
-
+ 
   }
     catch(Exception e)
     {
